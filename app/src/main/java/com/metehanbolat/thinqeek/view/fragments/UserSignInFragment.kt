79c761d5c57ef0bd846851ceb22352231ae82c1d
Metehan Bolat <metehan.bolat.ie@gmail.com>
2021-11-17 21:35:47 +0300

    Google Sign In process

--- app/src/main/java/com/metehanbolat/thinqeek/view/fragments/UserSignInFragment.kt
+++ app/src/main/java/com/metehanbolat/thinqeek/view/fragments/UserSignInFragment.kt
@@ -1,6 +1,9 @@
 package com.metehanbolat.thinqeek.view.fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,8 +14,13 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.CompositePageTransformer
 import androidx.viewpager2.widget.MarginPageTransformer
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
@@ -21,6 +29,7 @@ import com.metehanbolat.thinqeek.R
 import com.metehanbolat.thinqeek.adapter.SliderAdapter
 import com.metehanbolat.thinqeek.databinding.FragmentUserSignInBinding
 import com.metehanbolat.thinqeek.model.SliderItem
+import com.metehanbolat.thinqeek.view.activities.ApplicationActivity
 import com.metehanbolat.thinqeek.viewmodel.UserSignInFragmentViewModel
 import kotlin.math.abs
 
@@ -29,8 +38,13 @@ class UserSignInFragment : Fragment() {
     private var _binding: FragmentUserSignInBinding? = null
     private val binding get() = _binding!!
 
+    companion object{
+        private const val RC_SIGN_IN = 120
+    }
+
     private lateinit var viewModel: UserSignInFragmentViewModel
     private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
     private lateinit var firestore: FirebaseFirestore
     private lateinit var navController: NavController
 
@@ -48,11 +62,22 @@ class UserSignInFragment : Fragment() {
         firestore = Firebase.firestore
         viewPager2 = binding.viewPagerImageSlider
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        Handler().postDelayed({
+
+        },2000)
 
         val sliderItems: MutableList<SliderItem> = ArrayList()
 
@@ -90,6 +115,10 @@ class UserSignInFragment : Fragment() {
             }
         }
 
+        binding.button.setOnClickListener {
+            signIn()
+        }
+
         binding.signUpText.setOnClickListener {
             navController = findNavController()
             navController.navigate(R.id.action_userSignInFragment_to_userSignUpFragment)
@@ -109,6 +138,52 @@ class UserSignInFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val exception = task.exception
+            if (task.isSuccessful){
+                try {
+                    // Google Sign In was successful, authenticate with Firebase
+                    val account = task.getResult(ApiException::class.java)!!
+                    Log.d("SingInFragment", "firebaseAuthWithGoogle:" + account.id)
+                    firebaseAuthWithGoogle(account.idToken!!)
+                } catch (e: ApiException) {
+                    // Google Sign In failed, update UI appropriately
+                    Log.w("SingInFragment", "Google sign in failed", e)
+                }
+            }else{
+                Log.w("SingInFragment", exception.toString())
+                println("else çöktü")
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("SingInFragment", "signInWithCredential:success")
+                    val intent = Intent(requireContext(),ApplicationActivity::class.java)
+                    startActivity(intent)
+                    activity?.finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("SingInFragment", "signInWithCredential:failure", task.exception)
+                }
+            }
     }
 
     private fun visible(){
